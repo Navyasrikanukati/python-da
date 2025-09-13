859fde100b36ec95fb63f14ef8becc25299e8a3f
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-13 21:03:23 +0530

    DATATYPES OF NUMARICS

---
+++ numaric.py
@@ -0,0 +1,57 @@
+      # INT:--
+# x=1
+# print(x)
+# print(type(x))
+# y=-1
+# print(y)
+# print(type(y))
+# z=0
+# print(z)
+# print(type(z))
+# m=12
+# print(m)
+# print(type(m))
+# n=-5
+# print(n)
+# print(type(n))
+      #FLOAT:--
+a =0.1
+print(a)
+print(type(a))
+
+b=-1.0
+print(b)
+print(type(b))
+
+
+c=17.0
+print(c)
+print(type(c))
+
+
+
+d=123.8727
+print(d)
+print(type(d))
+
+
+e=-452.5
+print(print(type(e)))
+
+
+    #COMPLEX
+p=1+1j
+print(p)
+print(type(p))
+
+q=-1-1j
+print(q)
+print(type(q))
+
+r=0.0-0j
+print(r)
+print(type(r))
+
+s=1732+48j
+print(s)
+print(type(s))
