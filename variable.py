c2918e36bbb4fab6f1931f9de8619d7c42e9cec3
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-13 20:12:23 +0530

    variables

---
+++ variable.py
@@ -0,0 +1,58 @@
+#variables :-
+#  a container to store data 
+# a reference to access date 
+# a context to get data 
+
+
+# ex: myname 
+# my age 
+# email
+# salary 
+# myname = 'navya sri'
+# myage = 54
+# email = 'navyasri@gmail.com'
+# salary =5000
+# print(myname)
+# print(myage)
+# print(email)
+# print(salary)
+
+
+#   rules to create a variables:-
+
+# 1 variables can contain alphabets [upper case /lower case]
+# 2 variables can contain numbers but not at start of variable 
+# 3 variables should not contain special char
+# 4 variables should not  be a keyworld
+
+
+
+     # what is a keywords :-
+# keywords are nothing but those words 
+# python uses internally to operate
+
+# import keyword
+# print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as',
+# 'assert', 'async', 'await', 'break',
+# 'class', 'continue', 'def', 'del', 
+# 'elif', 'else', 'except', 'finally', 
+# 'for', 'from', 'global', 'if', 'import',
+# 'in', 'is', 'lambda', 'nonlocal', 'not',
+# 'or', 'pass', 'raise', 'return', 'try', 
+# 'while', 'with', 'yield']
+
+
+#exercise:-  
+fav_book = "hw to stop overthiking"
+_my_book ="python"
+MY_BOOK ="NAVYA"
+###############################
+NAME="navya"
+name="navya"
+###################################
+#30_ACRE ="property"
+Acre_30 ="property"
+###############
+3
+
