d0305c50d606993830f95367d1cb564cc3ab5078
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-14 11:34:59 +0530

    arithmetic operation

---
+++ arithmati_ops.py
@@ -0,0 +1,61 @@
+#ARITHMETIC OPERATIONS:-
+# 3+6=9
+
+# WHAT IS EXPRESSION ?
+# WHAT IS OPERATOR ?
+# WHAT IS OPERAND ?
+
+#1 ADDITION :-  +
+# x=10
+# y=2
+# print("ADDITION")
+# print(x+y)
+# #2 SUBSTRACTION :-
+# print("SUBSTRSACTION")
+# print(x-y)
+# #3 MULLTIPITLICATION:-
+# print("MULLTIPITLICATION")
+# print(x*y)
+# #4 DIVITION:-
+#  # 1 NORMAL DIVITION :-
+# print("NORMAL DIVITION")
+# print(x/y)
+
+#    #2 FLOOR DIVITION  5/2
+# print ("floor divition")
+# print(x//y)
+# # 5 EXPONENT/POWER:-**
+# print("exponet")
+# print(x**y)
+# # MOD OPERATION:- %
+# print("mod oparation")
+# print(x%y)
+
+# dicimal number
+# m=7.3
+# n=2.2
+# add
+# print(m+n)
+# sub
+# print(m-n)
+# muti
+# print(n*m)
+# normal div
+# print(m/n)
+# floor div
+# print(m//n)
+# mod op
+# print(m**n)
+# complex number:-
+p=4+8j
+q=2+4j
+print (f"addition of p and q is {p+q}")
+print(f"substraction of p and q is {p-q}")
+print(f"mutipcation of p and q is {p*q}")
+print(f"normal divistion of p and q is {p/q}")
+print(f"power of p and q  is {p**q}")
+#print(f"floor  divistion of p and q is {p//q}") this both are not wroking in complex datatype
+#print (f"mod oper of p and q is{p%q}")
+
+# f= fromate
+# {}= viriable
