b722a4092655bc2118d7d4b5a01d7094d97766a1
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-20 15:48:09 +0530

    tuple dt

---
+++ tuple.py
@@ -0,0 +1,81 @@
+# #TUPLE:-
+# Immutable
+# heterogenenous
+# ordered
+# indexed
+# used to storefix data
+
+
+
+
+
+# How to create initialize tuple:-
+
+# tup1=(1,2,True,'Bangaram','Buji',['a','b',0.5])
+# print(tup1)
+# print(type(tup1))
+
+
+# tup2=tuple([1,2,3,4,5])
+# print(tup2)
+# print(type(tup2))
+
+# tup3=tuple((10,'A',5,5,False))
+# print(tup3)
+# print(type(tup3))
+
+# tup4=tuple({50,60,70,'priya'})
+# print(tup4)
+# print(type(tup4))
+
+
+# tup5=tuple('APPLE')
+# print(tup5)
+# print(type(tup5))
+#############################
+#t1=(12,23,'navya',True,5.5,[1,2,3])
+# #+ve
+# print(t1)
+# print(t1[0])
+# print(t1[1])
+# print(t1[2])
+# #-ve
+
+# print(t1[-1])
+# print(t1[-2])
+# print(t1[-3])
+
+
+#slicing
+
+# print(t1[0:3:1])
+# print(t1[-4:-1:1])
+# print(t1[4:1:-1])
+# print(t1[2][2])
+# print(t1[2][3])
+
+#BUILLT METHODS
+
+#t2=(12,23,'navya',True,5.5,[1,2,3],12,8,'g',False,0,1)
+# #COUNT
+# print(t2.count(12))
+# print(t2.count(1))
+# print(t2.count(False))
+
+##index 
+# print(t2.index(5.5))
+# print(t2.index('g'))
+#addition
+# t1=(1,2,3)
+# t2=(4,5,6)
+# print(t1+t2)
+# #repitaion
+# t11=(1,2,3)
+# print(t11*3)
+#unpacking
+t100=(12,23,45)
+a,b,c=t100
+print(a)
+print(b)
+print(c)
+#print(d)
