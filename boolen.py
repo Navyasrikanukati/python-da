3542d89e93c0f6b8e62fb769fd580194f5920873
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-21 11:05:37 +0530

    boolen dt

---
+++ boolen.py
@@ -0,0 +1,8 @@
+#boolen datatypy has only two values
+# 1) True ---1 in mathametical tearms
+# 2) False---0 in mathametical tearms
+
+z='navyasri'
+print(z!='navya')
+print(z=='navya')
+print(z!='navyasri')
