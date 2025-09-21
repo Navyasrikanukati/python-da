61e5ce0b9b54eb5f216dab4033a8d02a92db961b
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-21 12:12:04 +0530

    set dt

---
+++ set dt.py
@@ -0,0 +1,63 @@
+# SET :-
+# #set is a collection 
+# set is collecction unique elements
+# set is mutable
+# set is unordered
+# doest allow duplicaties
+# it is partially heterogeneous
+# it doesnot support indexing
+
+
+# how to intialize  /cerating set:-
+# set1={1,2}
+# print(set1)
+# print(type(set1))
+
+# set2= set([1,2,3,4,5])
+# print(set2)
+# print(type(set2))
+
+# set3=set((4,5,6,7))
+# print(set3)
+# print(type(set3))
+
+# set4=set({20,30,40,40,50})
+# print(set4)
+# print(type(set4))
+
+# set5=set("apple")
+# print(set5)
+# print(type(set5))
+
+# #membership operator:-
+# in
+# not in
+#set100={"A",12,34.5,67,1.0,False,('a','c',100,True)}
+# print(120 in set100)
+# print(120 not in set100)
+# print('A'.lower()in set100)
+# print(1 in set100)
+# #print(0.0000 in set100)
+# #print (1 in set100)
+#  ##  ADDING ELEMENTS TO THE SET:-
+set420={12,23,34,45,56,'A','a',65}
+# # set420.add(67)
+# # print(set420)
+# # print(type(set420))
+# # set420.add("BABOON".title())
+# # print(set420)
+# # set420.add(False)
+# #print(set420)
+# set420.add((1,2,3))
+# print(set420)
+# REMOVING:-
+#set420.remove(False)
+#set420.remove(34)
+# #set420.remove(9*5)
+# set420.remove((True+True)*6)
+# print(set420)
+
+# set420.remove(chr(97))
+# print(set420)
+# set420.remove(ord('A'))
+# print(set420)
