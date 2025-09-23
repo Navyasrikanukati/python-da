42ba4a4a5b6376cdb1e13c337ff271c905d30fab
Navyasrikanukati <navyasrikanukati@gmail.com>
2025-09-23 15:31:50 +0530

    byted dt

---
+++ bytede.py
@@ -0,0 +1,34 @@
+# print(int("1001011",2))
+# print(chr(75))
+
+
+#byte datatype 
+#byte data type is a reprention ascii 
+#initialize a byte datatype with byte literl
+# b1=b'ABCD'
+# print(b1)
+
+# print(b1[0])
+# print(b1[1])
+# print(b1[2])
+# print(b1[3])
+
+
+
+# b2=b'8976'
+# print(b2)
+# print(b2[0])
+# print(b2[1])
+
+
+# b3=b"@#$"
+# print(b3[0])
+# print(b3[1])
+# print(b3[2])
+
+
+#
+# b10=bytes('ABCD','utf-8')
+# print(b10)
+# print(b10[0])
+# print(b10[-1])
